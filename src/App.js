4138ab142bafd1f6735c8b00bcf7fdaa61529235
Brandon Maroni <brandon-maroni@lambdastudents.com>
2020-09-11 17:39:31 -0600

    wrapped app return in Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <div>
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+      </div>
+    </Router>
   );
 };
 export default App;
